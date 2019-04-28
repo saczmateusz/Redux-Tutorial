e556b8dc99c3376e2b9e26780ea917c2b4f0d716
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-04-28 19:40:24 +0200

    newPost action implementation

--- src/reducers/actions/postActions.js
+++ src/reducers/actions/postActions.js
@@ -2,11 +2,25 @@ import { AXIOS_POST, NEW_POST } from "./types";
 import axios from "axios";
 
 export const axiosPost = () => dispatch => {
-  console.log("im here tho");
   axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
     dispatch({
       type: AXIOS_POST,
       payload: response.data
     });
   });
+};
+
+export const createPost = postData => dispatch => {
+  axios
+    .post("https://jsonplaceholder.typicode.com/posts", postData, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      dispatch({
+        type: NEW_POST,
+        payload: response.data
+      });
+    });
 };
