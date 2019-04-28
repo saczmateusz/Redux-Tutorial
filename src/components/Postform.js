d7021759eb2f8c73a5f0a7a2644c0a1ab1537193
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-04-28 15:49:53 +0200

    Update Postform.js

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,7 +1,51 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Postform extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    axios.post().then();
+  };
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1> Add Post</h1>
+        <form>
+          <div>
+            <label>Title: </label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <br />
+          <div>
+            <label>Body: </label>
+            <br />
+            <textarea
+              name="body"
+              onChange={this.onChange}
+              value={this.state.body}
+            />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
