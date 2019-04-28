0f013ef7bee8d83ef91ff5572fd810cd213befbf
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-04-28 18:44:13 +0200

    app uses redux now

--- src/App.js
+++ src/App.js
@@ -5,28 +5,31 @@ import { Provider } from "react-redux";
 
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Postform />
-        <hr />
-        <Posts />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Postform />
+          <hr />
+          <Posts />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
