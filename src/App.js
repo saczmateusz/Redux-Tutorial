eb317d5039dbcaa85b49eddca5e598ec61d05855
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-04-28 15:50:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
 
+import Posts from "./components/Posts";
+import Postform from "./components/Postform";
+
 function App() {
   return (
-    <Provider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Postform />
+        <hr />
+        <Posts />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
